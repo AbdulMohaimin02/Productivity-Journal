91853c7b1efa4d4381bfa58581d968933a556807
Abdul Mohaimin <abdul.mohaimin@thebeansgroup.com>
2022-03-31 11:57:32 +0100

    Main screen now reads all the notes from data base

--- data/data.js
+++ data/data.js
@@ -1,6 +1,6 @@
 import { getJSON } from "./utils.js";
 
-const API = "";
+const API = "http://localhost:3000/notes";
 
 let notes;
 
@@ -10,5 +10,6 @@ try {
 } catch (error) {
   notes = error;
 }
+
 
 export { notes as default, API };
--- main_screen/index.js
+++ main_screen/index.js
@@ -1,6 +1,37 @@
 import DOMPurify from "https://cdn.jsdelivr.net/npm/dompurify@2/dist/purify.es.min.js";
 // import { getJSON, serialize, stringToHTML } from "./utils.js";
-// import notes, { API } from "./data.js";
+import notes, { API } from "../data/data.js";
+
+
+const app = document.querySelector("#myNotes");
+
+function main() {
+  if (notes instanceof Error) {
+    app.textContent = "There was a problem. Try again later.";
+    console.error(notes);
+  } else {
+
+    // Insert the HTML string
+    app.innerHTML = `
+        ${DOMPurify.sanitize(notes.map(getNotesHTML).join(""))}
+    `;
+  }
+}
+
+function getNotesHTML({ id, name, info, createdAt, updatedAt}) {
+  return `
+  <div class="card">
+    <div class="buttonStyle">
+      <h4>${name}</h4>
+        <button type="button" class="exitButton" aria-label="Close '${name}'"
+          data-close="">&times;</button>
+    </div>
+    <p> ${info}</p>
+  </div>
+  `;
+}
+
+
 
 
 
@@ -43,6 +74,9 @@ const menu_toggle = document.querySelector('.menu-toggle')
 const sidebar = document.querySelector('.sidebar');
 
 menu_toggle.addEventListener('click', () => {
-    menu_toggle.classList.toggle('is-active');
-    sidebar.classList.toggle('is-active');
+  menu_toggle.classList.toggle('is-active');
+  sidebar.classList.toggle('is-active');
 })
+
+
+main();
