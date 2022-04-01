37737031cd70b38abbf77cc8d511852412a0b130
Abdul Mohaimin <abdul.mohaimin@thebeansgroup.com>
2022-04-01 12:21:43 +0100

    Changed file structure to fix github pages error

--- nav.js
+++ nav.js
@@ -9,12 +9,14 @@ fetch("https://ad-productivity-app.herokuapp.com/notes")
     menu.innerHTML += notes
       .map(({ id, name }) => {
         return `
-        <a class="menu-item" href="./viewing_screen/view.html?id=${id}">
+        <a class="menu-item" href="./view.html?id=${id}">
           ${name}
         </a>`;
       })
       .join("");
   });
+
+
 
 menu_toggle.addEventListener("click", () => {
   menu_toggle.classList.toggle("is-active");
